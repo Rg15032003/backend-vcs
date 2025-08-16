1c6f7ffaeb2f9bfd7184fb8b6df3a39dccd30097
Rahul Gupta <rg15032003@gmail.com>
2025-08-17 02:00:51 +0530

    added profile,navbar and heatmap

--- routes/user.router.js
+++ routes/user.router.js
@@ -7,7 +7,7 @@ const userRouter=express.Router();
 userRouter.get("/allUsers",userController.getAllUsers);
 userRouter.post("/signup",userController.signup);
 userRouter.post("/login",userController.login);
-userRouter.get("/getUserProfile/:id",userController.getUserProfile);
+userRouter.get("/userProfile/:id",userController.getUserProfile);
 userRouter.put("/updateProfile/:id",userController.updateUserProfile);
 userRouter.delete("/deleteProfile/:id",userController.deleteUserProfile);
 
